ea8be9c62806285052be5b2add83b91423186962
Sergey Vergun <sergey@vergun.in.ua>
2021-09-23 00:28:05 +0300

    fixed updating hosts file and installing packages

--- zookeeper-standalone/provision.sh
+++ zookeeper-standalone/provision.sh
@@ -2,17 +2,30 @@
 
 # Configure /etc/hosts
 echo "CONFIGURE /etc/hosts ..."
-grep "192.0.2.101 node1.example.com node1" || echo "192.0.2.101 node1.example.com node1" >> /etc/hosts
+grep -xF "192.0.2.101 node1.example.com node1" /etc/hosts || echo "192.0.2.101 node1.example.com node1" >> /etc/hosts
 
 
 # Install packages
 echo "INSTALL PACKAGES ..."
 PACKAGE_LIST="git lsof mc net-tools tar telnet tree vim wget zip"
 
-if [ -x "$(command -v apt-get)" ]; then sudo apt-get install -y -q $PACKAGE_LIST openjdk-11-jdk nmap
-  elif [ -x "$(command -v dnf)" ]; then sudo dnf install -y -q $PACKAGE_LIST epel-release bind-utils java-11-openjdk nmap-ncat
-  elif [ -x "$(command -v yum)" ]; then sudo zypper install -y -q $PACKAGE_LIST epel-release bind-utils java-11-openjdk nmap-ncat
-else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $PACKAGE_LIST">&2; fi
+if [ -x "$(command -v apt-get)" ]; then
+    echo "--> Add universe repository"
+    add-apt-repository -y universe
+    echo "--> Install packages"
+    apt-get install -y -q openjdk-11-jdk nmap $PACKAGE_LIST
+  elif [ -x "$(command -v dnf)" ]; then
+    echo "--> Install EPEL Release"
+    dnf install -y -q epel-release
+    echo "--> Install packages"
+    dnf install -y -q bind-utils java-11-openjdk nmap-ncat $PACKAGE_LIST
+  elif [ -x "$(command -v yum)" ]; then
+    echo "--> Install EPEL Release"
+    yum install -y -q epel-release
+    echo "--> Install packages"
+    yum install -y -q bind-utils java-11-openjdk nmap-ncat $PACKAGE_LIST
+  else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $PACKAGE_LIST">&2
+fi
 
 
 # Zookeeper provision
